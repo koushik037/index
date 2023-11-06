2a682d409d5fb7e8c4a0a0f3fb8000a44e76a61a
koushik <koushik037@gmail.com>
2023-11-06 11:01:22 +0530

    2nd try to push env

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const { default: axios } = require('axios')
 const express = require('express')
 const {createProxyMiddleware} =require('http-proxy-middleware')
-const puppeteer =require('puppeteer')
+require('dotenv').config()
+
+const port = process.env.PORT || '3001'
+
+
 const app =express()
 app.get('/',(req,res)=>{
   res.send('hello world 1')
 })
 
-app.listen('5222',()=>{console.log('server start at 5222')})
+app.listen(port,()=>{console.log(`server start at ${port}`)})
